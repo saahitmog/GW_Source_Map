256346ba3ba2e985dbf933b6d34ea5572d81bd04
Saahit Mogan <saahitsmogan@gmail.com>
2024-12-12 15:13:42 -0800

    recreating Zou et al. plots

---
+++ data/preprocess.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 'Filtering Dataset ...'
+fitscopy 'desidr9_galaxy_cspcat.fits[Joined][spec_z > -10 && MASS_BEST > 0.0]' \!desidr9_galaxy_cspcat_cut.fits
+echo 'Filtered Dataset Created'
